e9d98652d1ac3146d25eef429dd64e5bdbf5ad57
BrinkyPinky <brinkypinky3@gmail.com>
2022-09-30 16:52:09 +0500

    Environment done

--- Dino/GameScene.swift
+++ Dino/GameScene.swift
@@ -21,6 +21,30 @@ class GameScene: SKScene {
     // MARK: Move Environment
     
     func moveEnvironment() {
+        enumerateChildNodes(withName: "BackgroundLayer7") { [unowned self] node, _ in
+            moveNode(shift: 0.9, node: node)
+        }
+        
+        enumerateChildNodes(withName: "BackgroundLayer6") { [unowned self] node, _ in
+            moveNode(shift: 1.1, node: node)
+        }
+        
+        enumerateChildNodes(withName: "LightsBackgroundLayer5") { [unowned self] node, _ in
+            moveNode(shift: 1.3, node: node)
+        }
+        
+        enumerateChildNodes(withName: "BackgroundLayer4") { [unowned self] node, _ in
+            moveNode(shift: 1.4, node: node)
+        }
+        
+        enumerateChildNodes(withName: "BackgroundLayer3") { [unowned self] node, _ in
+            moveNode(shift: 1.6, node: node)
+        }
+        
+        enumerateChildNodes(withName: "LightsBackgroundLayer2") { [unowned self] node, _ in
+            moveNode(shift: 1.7, node: node)
+        }
+        
         enumerateChildNodes(withName: "BackgroundLayer1") { [unowned self] node, _ in
             moveNode(shift: 1.8, node: node)
         }
@@ -32,6 +56,7 @@ class GameScene: SKScene {
         enumerateChildNodes(withName: "ForegroundGround") { [unowned self] node, _ in
             moveNode(shift: 2.3, node: node)
         }
+        
     }
 }
 
@@ -43,11 +68,56 @@ extension GameScene {
         for i in 0..<2 {
             createBackgroundNode(
                 number: i,
-                nodeName: "Ground",
-                imageName: "ground",
-                height: self.scene!.size.width/6.5,
-                zPosition: 0,
-                yPosition: -(self.scene?.size.height)!/2
+                nodeName: "BackgroundLayer7",
+                imageName: "backgroundLayer7",
+                height: self.scene!.size.width/2.3,
+                zPosition: -7,
+                yPosition: -60
+            )
+            
+            createBackgroundNode(
+                number: i,
+                nodeName: "BackgroundLayer6",
+                imageName: "backgroundLayer6",
+                height: 170,
+                zPosition: -6,
+                yPosition: -45
+            )
+            
+            createBackgroundNode(
+                number: i,
+                nodeName: "LightsBackgroundLayer5",
+                imageName: "lightsBackgroundLayer5",
+                height: self.scene!.size.width/3,
+                zPosition: -5,
+                yPosition: -(self.scene!.size.height)/100
+            )
+            
+            createBackgroundNode(
+                number: i,
+                nodeName: "BackgroundLayer4",
+                imageName: "backgroundLayer4",
+                height: 170,
+                zPosition: -4,
+                yPosition: -45
+            )
+            
+            createBackgroundNode(
+                number: i,
+                nodeName: "BackgroundLayer3",
+                imageName: "backgroundLayer3",
+                height: 170,
+                zPosition: -3,
+                yPosition: -45
+            )
+            
+            createBackgroundNode(
+                number: i,
+                nodeName: "LightsBackgroundLayer2",
+                imageName: "lightsBackgroundLayer2",
+                height: self.scene!.size.width/3,
+                zPosition: -2,
+                yPosition: -(self.scene?.size.height)!/100
             )
             
             createBackgroundNode(
@@ -57,6 +127,15 @@ extension GameScene {
                 height: 315,
                 zPosition: -1,
                 yPosition: 35
+            )
+            
+            createBackgroundNode(
+                number: i,
+                nodeName: "Ground",
+                imageName: "ground",
+                height: self.scene!.size.width/6.5,
+                zPosition: 0,
+                yPosition: -(self.scene?.size.height)!/2
             )
             
             createBackgroundNode(
